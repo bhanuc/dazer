235763e9a4fd83de1e7a562c02603efda1a07349
bhanuc <bhanuc@iitk.ac.in>
2014-02-07 00:52:21 +0530

    First dazer commit, jsut hello world

---
+++ dazer.js
@@ -0,0 +1,26 @@
+//This file has all the configurations of the app such as each and every aspect of the framework
+
+var config = require('config/app'); 
+
+//Start koa
+var koa = require('koa');
+var app = koa();
+
+if(config.controller.need){
+ //    activate the controllers
+};
+if(config.model.need){
+    // activate the models
+};
+if(config.policy.need){
+    //activate the models
+};
+if(config.view.need){
+    //activate the views
+};
+
+app.use(function *(){
+  this.body = 'Hello World Form Dazer';
+});
+
+if (!module.parent) app.listen(3000);
