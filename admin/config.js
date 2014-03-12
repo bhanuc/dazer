f6e597d0bb278f7b84bef0b8bdc389f9c54eb194
bhanc <bhanuc@iitk.ac.in>
2014-03-12 11:51:50 +0530

    added location config

---
+++ admin/config.js
@@ -0,0 +1,10 @@
+//==================================================
+//Stores the location of model,views and controller and other settings that might be required later. Here false means no directory has been chosen
+//==================================================
+module.exports = {
+    controller_dir: 'false',
+    model_dir: 'false',
+    views_dir: 'false',
+    public_dir: 'false'
+
+};
